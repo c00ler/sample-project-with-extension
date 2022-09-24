6eb5f5d46ba1309d61035721bbabc43f79eeb1a6
Alexey Venderov <avenderov@gradle.com>
2022-09-24 11:24:24 +0200

    Add event spy. Apply extension via extensions.xml

---
+++ simple-extension/src/main/java/com/github/sample/extension/SimpleEventSpy.java
@@ -0,0 +1,29 @@
+package com.github.sample.extension;
+
+import org.apache.maven.eventspy.AbstractEventSpy;
+import org.apache.maven.eventspy.EventSpy;
+import org.codehaus.plexus.component.annotations.Component;
+import org.codehaus.plexus.component.annotations.Requirement;
+import org.codehaus.plexus.logging.Logger;
+import org.slf4j.LoggerFactory;
+
+@Component(role = EventSpy.class)
+public class SimpleEventSpy extends AbstractEventSpy {
+
+    private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(SimpleEventSpy.class);
+
+    @Requirement
+    private Logger plexusLogger;
+
+    @Override
+    public void init(Context context) throws Exception {
+        LOGGER.debug("Simple :: spy :: init :: slf4j");
+        plexusLogger.debug("Simple :: spy :: init :: plexus");
+    }
+
+    @Override
+    public void close() throws Exception {
+        LOGGER.debug("Simple :: spy :: close :: slf4j");
+        plexusLogger.debug("Simple :: spy :: close :: plexus");
+    }
+}
--- simple-extension/src/main/java/com/github/sample/extension/SimpleLifecycleParticipant.java
+++ simple-extension/src/main/java/com/github/sample/extension/SimpleLifecycleParticipant.java
@@ -8,17 +8,17 @@ import org.codehaus.plexus.component.annotations.Requirement;
 import org.codehaus.plexus.logging.Logger;
 import org.slf4j.LoggerFactory;
 
-@Component(role = AbstractMavenLifecycleParticipant.class, hint = "simple")
-public class SimpleMavenLifecycleParticipant extends AbstractMavenLifecycleParticipant {
+@Component(role = AbstractMavenLifecycleParticipant.class)
+public class SimpleLifecycleParticipant extends AbstractMavenLifecycleParticipant {
 
-    private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(SimpleMavenLifecycleParticipant.class);
+    private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(SimpleLifecycleParticipant.class);
 
     @Requirement
     private Logger plexusLogger;
 
     @Override
     public void afterSessionEnd(MavenSession session) throws MavenExecutionException {
-        LOGGER.debug("Simple :: Slf4j");
-        plexusLogger.debug("Simple :: Plexus");
+        LOGGER.debug("Simple :: lifecycleParticipant :: afterSessionEnd :: slf4j");
+        plexusLogger.debug("Simple :: lifecycleParticipant :: afterSessionEnd :: plexus");
     }
 }
